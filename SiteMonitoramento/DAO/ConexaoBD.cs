3b0cce7e5b8d060bf26a6e5eadf77fd33cd0a910
Henrico Birochi <henrico.birochi@gmail.com>
2025-03-28 01:55:38 -0300

    Configurações iniciais, pasta DAO adicionada e classes ConexaoBD e HelperDAO adicionadas

---
+++ SiteMonitoramento/DAO/ConexaoBD.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace SiteMonitoramento.DAO
+{
+    public class ConexaoBD
+    {
+        public static SqlConnection GetConexao()
+        {
+            string strCon = "Data Source=LOCALHOST;Initial Catalog=AulaDB;User Id=sa;Password=123456;TrustServerCertificate=True;";
+            SqlConnection conexao = new SqlConnection(strCon);
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
---
+++ SiteMonitoramento/DAO/HelperDAO.cs
@@ -0,0 +1,63 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace SiteMonitoramento.DAO
+{
+    public class HelperDAO
+    {
+        public static void ExecutaSQL(string sql, SqlParameter[] parametros)
+        {
+            using (SqlConnection cnx = ConexaoBD.GetConexao())
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, cnx))
+                {
+                    if (parametros != null)
+                        cmd.Parameters.AddRange(parametros);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public static DataTable ExecutaSelect(string sql, SqlParameter[] parametros)
+        {
+            using (SqlConnection cnx = ConexaoBD.GetConexao())
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(sql, cnx))
+                {
+                    if (parametros != null)
+                        adapter.SelectCommand.Parameters.AddRange(parametros);
+                    DataTable tabela = new DataTable();
+                    adapter.Fill(tabela);
+                    return tabela;
+                }
+            }
+        }
+        public static void ExecutaProc(string nomeProc, SqlParameter[] parametros)
+        {
+            using (SqlConnection cnx = ConexaoBD.GetConexao())
+            {
+                using (SqlCommand comando = new SqlCommand(nomeProc, cnx))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    if (parametros != null)
+                        comando.Parameters.AddRange(parametros);
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+        public static DataTable ExecutaProcSelect(string nomeProc, SqlParameter[] parametros)
+        {
+            using (SqlConnection cnx = ConexaoBD.GetConexao())
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(nomeProc, cnx))
+                {
+                    if (parametros != null)
+                        adapter.SelectCommand.Parameters.AddRange(parametros);
+                    adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    DataTable tabela = new DataTable();
+                    adapter.Fill(tabela);
+                    return tabela;
+                }
+            }
+        }
+    }
+}
