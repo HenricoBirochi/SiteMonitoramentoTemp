d7c84966321d62b10dabd1dc19b448ab216d4f4d
Henrico Birochi <henrico.birochi@gmail.com>
2025-03-29 01:04:39 -0300

    Pasta do banco de dados adicionada, foram criadas as classes UsuarioEmpresa, UsuarioPessoa, a interface IUsuario e a pasta ViewModels foi criada

---
+++ SiteMonitoramento/Models/Interfaces/IUsuario.cs
@@ -0,0 +1,9 @@
+﻿namespace SiteMonitoramento.Models.Interfaces
+{
+    public interface IUsuario
+    {
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ SiteMonitoramento/Models/UsuarioPessoa.cs
@@ -0,0 +1,13 @@
+﻿using SiteMonitoramento.Models.Interfaces;
+
+namespace SiteMonitoramento.Models
+{
+    public class UsuarioPessoa : IUsuario
+    {
+        public int UsuarioPessoaId { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+    }
+}
