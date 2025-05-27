0da792ab89dc21a32f4a741a3ae792cbe4bf7e7e
HENRICO BIROCHI <081230027@faculdade.cefsa.edu.br>
2025-05-27 20:59:49 -0300

    Adicionando MetodoPost para a API

--- SiteMonitoramento/Controllers/DispositivoController.cs
+++ SiteMonitoramento/Controllers/DispositivoController.cs
@@ -2,6 +2,8 @@
 using SiteMonitoramento.DAO;
 using SiteMonitoramento.Models;
 using System;
+using System.Net;
+using System.Net.Http;
 
 namespace SiteMonitoramento.Controllers
 {
@@ -39,5 +41,64 @@ namespace SiteMonitoramento.Controllers
                 ModelState.AddModelError("DispositivoNome", "O nome do dispositivo precisa ser preenchido");
         }
 
+        public override IActionResult Save(Dispositivo model, string Operacao)
+        {
+            try
+            {
+
+                ValidaDados(model, Operacao);
+                if (ModelState.IsValid == false)
+                {
+                    ViewBag.Operacao = Operacao;
+                    PreencheDadosParaView(Operacao, model);
+                    return View(NomeViewForm, model);
+                }
+                else
+                {
+                    if (Operacao == "I")
+                    {
+                        DAO.Inserir(model);
+                        CriaDispositivoMongo();
+                    }
+                    else
+                        DAO.Alterar(model);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception erro)
+            {
+                return View("Error", new ErrorViewModel(erro.ToString()));
+            }
+
+        }
+        public IActionResult CriaDispositivoMongo()
+        {
+            var dao = new DispositivoDAO();
+            int idUltimoDisp = dao.ProximoId();
+            string domain = "localhost:8080"; //Depois mudar para o ip da VM na AWS
+            string url = $"http://{domain}:4041/iot/devices";
+            var content = new StringContent("{\n  \"devices\": [\n    {\n      \"device_id\": \"temp" + idUltimoDisp + "\",         \n      \"entity_name\": \"urn:ngsi-ld:Temp:" + idUltimoDisp + "\",   \n      \"entity_type\": \"Temp\",          \n      \"protocol\": \"PDI-IoTA-UltraLight\",  \n      \"transport\": \"MQTT\",            \n\n      \n      \"commands\": [\n        { \"name\": \"on\", \"type\": \"command\" },  \n        { \"name\": \"off\", \"type\": \"command\" }  \n      ],\n\n      \n      \"attributes\": [\n        { \"object_id\": \"s\", \"name\": \"state\", \"type\": \"Text\" }, \n        { \"object_id\": \"t\", \"name\": \"temperature\", \"type\": \"Number\" }  \n      ]\n    }\n  ]\n}\n", null, "application/json");
+            
+            //var handler = new HttpClientHandler();
+            using (var httpClient = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, url);
+                request.Headers.Add("fiware-service", "smart");
+                request.Headers.Add("fiware-servicepath", "/");
+                request.Content = content;
+                using (var response = httpClient.SendAsync(request).Result)
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        string resposta = response.Content.ReadAsStringAsync().Result;
+                        return Content(resposta);
+                    }
+                    else
+                    {
+                        throw new Exception("Erro ao consultar. Code: " + response.StatusCode);
+                    }
+                }
+            }
+        }
     }
 }
